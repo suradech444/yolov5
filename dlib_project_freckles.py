f8ad1bde2a8e3bf42da1d2cc0aaa424063aa6f2a
kongkoliizz <74130875+kongkoliizz@users.noreply.github.com>
2022-04-26 22:19:21 +0700

    te

--- dlib_project_freckles.py
+++ dlib_project_freckles.py
@@ -1,9 +1,78 @@
 import numpy as np
 import cv2
 from skimage.io import imsave, imread
+from pickle import NONE
+import dlib
+import matplotlib.pyplot as plt
 
 # step 1 - load the model
 
+face_detector = dlib.get_frontal_face_detector()
+
+datFile =  "PROJECT/content/shape_predictor_68_face_landmarks.dat"
+landmark_detector = dlib.shape_predictor(datFile)
+
+img_path = "test/3.jpg"
+ 
+#read with dlib
+img = dlib.load_rgb_image(img_path)
+
+faces = face_detector(img, 1)
+
+landmark_tuple = []
+for k, d in enumerate(faces):
+   landmarks = landmark_detector(img, d)
+   for n in range(0, 27):
+      x = landmarks.part(n).x
+      y = landmarks.part(n).y
+      landmark_tuple.append((x, y))
+      cv2.circle(img, (x, y), 2, (255, 255, 0), -1)
+
+routes = []
+ 
+for i in range(15, -1, -1):
+   from_coordinate = landmark_tuple[i+1]
+   to_coordinate = landmark_tuple[i]
+   routes.append(from_coordinate)
+ 
+from_coordinate = landmark_tuple[0]
+to_coordinate = landmark_tuple[17]
+routes.append(from_coordinate)
+ 
+for i in range(17, 20):
+   from_coordinate = landmark_tuple[i]
+   to_coordinate = landmark_tuple[i+1]
+   routes.append(from_coordinate)
+ 
+from_coordinate = landmark_tuple[19]
+to_coordinate = landmark_tuple[24]
+routes.append(from_coordinate)
+ 
+for i in range(24, 26):
+   from_coordinate = landmark_tuple[i]
+   to_coordinate = landmark_tuple[i+1]
+   routes.append(from_coordinate)
+ 
+from_coordinate = landmark_tuple[26]
+to_coordinate = landmark_tuple[16]
+routes.append(from_coordinate)
+routes.append(to_coordinate)
+
+for i in range(0, len(routes)-1):
+   from_coordinate = routes[i]
+   to_coordinate = routes[i+1]
+   img = cv2.line(img, from_coordinate, to_coordinate, (255, 255, 0), 1)
+
+mask = np.zeros((img.shape[0], img.shape[1]))
+mask = cv2.fillConvexPoly(mask, np.array(routes), 1)
+mask = mask.astype(np.bool)
+ 
+out = np.zeros_like(img)
+out[mask] = img[mask]
+
+imsave('PROJECT/file.jpg',out)
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 net = cv2.dnn.readNet('w2/best.onnx')
 
@@ -17,7 +86,7 @@ def format_yolov5(frame):
     result[0:row, 0:col] = frame
     return result
 
-image = cv2.imread('test/3.jpg')
+image = cv2.imread('PROJECT/file.jpg')
 input_image = format_yolov5(image) # making the image square
 blob = cv2.dnn.blobFromImage(input_image , 1/255.0, (640, 640), swapRB=True)
 net.setInput(blob)
